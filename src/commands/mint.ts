c35feee1c46be92fc451b2549af61fa6ee6d99a9
Egge <egge@getcurrent.io>
2024-10-12 09:11:33 +0200

    removed try/catch

--- src/commands/mint.ts
+++ src/commands/mint.ts
@@ -11,25 +11,21 @@ export async function mintHandler() {
   const mint = new CashuMint(mintUrl);
   const wallet = new CashuWallet(mint);
 
-  try {
-    const quote = await wallet.createMintQuote(21);
-    while (true) {
-      console.log("Awaiting payment...");
-      const state = await wallet.checkMintQuote(quote.quote);
-      if (state.state === "PAID") {
-        console.log(state);
-        break;
-      }
-      await sleep(2000);
+  const quote = await wallet.createMintQuote(21);
+  while (true) {
+    console.log("Awaiting payment...");
+    const state = await wallet.checkMintQuote(quote.quote);
+    if (state.state === "PAID") {
+      console.log(state);
+      break;
     }
-    console.log("Quote has been paid");
-    const { proofs } = await wallet.mintTokens(21, quote.quote);
-    const tokenData: Token = {
-      memo: "Demo",
-      token: [{ mint: mintUrl, proofs }],
-    };
-    console.log(getEncodedTokenV4(tokenData));
-  } catch (e) {
-    console.error(e);
+    await sleep(2000);
   }
+  console.log("Quote has been paid");
+  const { proofs } = await wallet.mintTokens(21, quote.quote);
+  const tokenData: Token = {
+    memo: "Demo",
+    token: [{ mint: mintUrl, proofs }],
+  };
+  console.log(getEncodedTokenV4(tokenData));
 }
